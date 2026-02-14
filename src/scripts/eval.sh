2b02214bef07645ce104d842757c3255ee829f15
Jian Lang <88083695+Jian-Lang@users.noreply.github.com>
2026-02-14 17:53:34 +0800

    Change dataset to 'hatememes' in eval script
    Updated dataset name and parameters for evaluation.

--- src/scripts/eval.sh
+++ src/scripts/eval.sh
@@ -1,15 +1,1 @@
-
-
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=0
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=2
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=3
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=4
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=5
-
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=6
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=7
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=8
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=9
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='image' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=10
-
-python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='mmimdb' data_para.missing_type='both' data_para.missing_rate=0.7 data_para.k=3 device='cuda:0' model_para.prompt_pos=0
+python src/main.py --config-name=ragpt_config optim_para.weight_decay=5e-5 data_para.dataset_name='hatememes' data_para.missing_type='text' data_para.missing_rate=0.7 data_para.k=5 device='cuda:0'
